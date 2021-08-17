4d965948bda6c46c8081887994112b2e4ccf6ab2
Nikita Konev <nikit.cpp@yandex.ru>
2021-08-17 05:27:28 +0300

    initial session support in redis
    rm

--- src/SandboxApplication.kt
+++ src/SandboxApplication.kt
@@ -6,8 +6,15 @@ import io.ktor.http.*
 import io.ktor.jackson.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import io.ktor.server.cio.*
-import io.ktor.server.engine.*
+import io.ktor.sessions.*
+import io.ktor.utils.io.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import redis.clients.jedis.HostAndPort
+import redis.clients.jedis.Jedis
+import java.io.ByteArrayOutputStream
+import kotlin.coroutines.coroutineContext
+
 
 /**
  * Main entrypoint of the executable that starts a Netty webserver at port 8080
@@ -20,6 +27,63 @@ fun main(args: Array<String>): Unit = io.ktor.server.cio.EngineMain.main(args)
 
 data class Customer(val id: Int, val firstName: String, val lastName: String)
 
+data class UserSession(val id: String, val count: Int)
+
+abstract class SimplifiedSessionStorage : SessionStorage {
+    abstract suspend fun read(id: String): String?
+    abstract suspend fun write(id: String, data: String?): Unit
+
+    override suspend fun invalidate(id: String) {
+        write(id, null)
+    }
+
+    override suspend fun <R> read(id: String, consumer: suspend (ByteReadChannel) -> R): R {
+        val data = read(id) ?: throw NoSuchElementException("Session $id not found")
+        return consumer(ByteReadChannel(data))
+    }
+
+    override suspend fun write(id: String, provider: suspend (ByteWriteChannel) -> Unit) {
+        return provider(CoroutineScope(Dispatchers.IO).reader(coroutineContext, autoFlush = true) {
+            write(id, channel.readAvailable())
+        }.channel)
+    }
+}
+
+suspend fun ByteReadChannel.readAvailable(): String {
+    val data = ByteArrayOutputStream()
+    val temp = ByteArray(1024)
+    while (!isClosedForRead) {
+        val read = readAvailable(temp)
+        if (read <= 0) break
+        data.write(temp, 0, read)
+    }
+    return String(data.toByteArray())
+}
+
+class RedisSessionStorage : SimplifiedSessionStorage {
+
+    private val jedis: Jedis
+
+    constructor() {
+        jedis = Jedis(HostAndPort("localhost", 36379))
+    }
+
+    override suspend fun read(id: String): String? {
+        return jedis.get(id)
+    }
+
+    override suspend fun write(id: String, data: String?) {
+        jedis.set(id, data)
+    }
+
+    override suspend fun invalidate(id: String) {
+        jedis.del(id)
+    }
+
+
+}
+
+
 /**
  * Module that just registers the root path / and replies with a text.
  */
@@ -27,12 +91,31 @@ fun Application.module() {
     install(ContentNegotiation) {
         jackson()
     }
+    install(Sessions) {
+        cookie<UserSession>("user_session", storage = RedisSessionStorage())
+    }
     routing {
         get("/") {
             call.respondText("Hello World!")
         }
         post("/customer") {
             call.respond(Customer(1, "Jet", "Brains"))
+        }
+        get("/login") {
+            call.sessions.set(UserSession(id = "123abc", count = 0))
+            call.respondRedirect("/")
+        }
+        get("/session") {
+            val userSession: UserSession? = call.sessions.get<UserSession>()
+            if (userSession != null) {
+                call.respond(userSession)
+            } else {
+                call.respond(HttpStatusCode.Gone)
+            }
+        }
+        get("/logout") {
+            call.sessions.clear<UserSession>()
+            call.respondRedirect("/")
         }
     }
 }
